e9b521a39f2b0ecb5e4d6b379c6f624543aa5b2a
lcasan <lcasan120100@gmail.com>
2024-11-04 02:43:43 -0500

    CORS global configuration

--- src/main/java/com/restfulapi/restful_api/RestfulApiApplication.java
+++ src/main/java/com/restfulapi/restful_api/RestfulApiApplication.java
@@ -1,7 +1,11 @@
 package com.restfulapi.restful_api;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class RestfulApiApplication {
@@ -9,5 +13,20 @@ public class RestfulApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApiApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                
+				registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 
 }
